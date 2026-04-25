ca2c97845366ca01d5ac0d1f39bde3f34f703073
Ikram <91834989+ikram-backend2324@users.noreply.github.com>
2026-04-25 20:16:13 +0500

    Change theme from 'darkly' to 'cyborg'

--- karakalpak_ner/settings.py
+++ karakalpak_ner/settings.py
@@ -155,7 +155,7 @@ JAZZMIN_UI_TWEAKS = {
     "sidebar_nav_compact_style": False,
     "sidebar_nav_legacy_style": False,
     "sidebar_nav_flat_style": False,
-    "theme": "darkly",
+    "theme": "cyborg",
     "dark_mode_theme": "darkly",
     "button_classes": {
         "primary":   "btn-primary",
